b2e97ad6bf1cde1f5646697b874f7eaa1c926c68
LAKHAL Hamza <46263489+hamzalak@users.noreply.github.com>
2019-04-16 01:15:57 +0200

    Update WebConfig.java

--- reservation/src/main/java/bus/reservation/templ/WebConfig.java
+++ reservation/src/main/java/bus/reservation/templ/WebConfig.java
@@ -3,7 +3,8 @@ package bus.reservation.templ;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//  Cette Classe n'as pas de rôle dans le code 
+//  Un tentative de generations du ticket sous format PDF et envoyer par mail
 @Configuration
  public class WebConfig implements WebMvcConfigurer {
 
